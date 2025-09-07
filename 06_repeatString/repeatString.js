64a2d23f78b079aa058acd255efd263ec550ea27
DevSergioSifuentes <dev.sergiosifuentes@gmail.com>
2025-09-06 22:14:44 -0600

    build: coded repeatString exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let longString = string;
+    if(num<0){
+        return "ERROR"
+    }else if(num===0){
+        return "";
+    }else{
 
+        for(let i=0;i<num-1;i++){
+            longString += string;
+        }
+    return longString
+    }
 };
 
 // Do not edit below this line
