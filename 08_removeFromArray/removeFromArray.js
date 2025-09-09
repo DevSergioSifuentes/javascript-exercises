6cc8a881e4d3d2d719753828905558aeb7e8dbb3
DevSergioSifuentes <dev.sergiosifuentes@gmail.com>
2025-09-08 23:03:29 -0600

    build: removeFRomArray function.
    funciotn removes values listed after an array,
    both numbers and strings. Used double for cycle

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...numValues) {
+    for(let i=0;i<numValues.length; i++){
+        
+        for(let j=originalArray.length-1;j >= 0; j--){
+            
+            if(originalArray[j] === numValues[i]){
+                originalArray.splice(j,1);
+            }
+        }
+    }
+    return originalArray;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
