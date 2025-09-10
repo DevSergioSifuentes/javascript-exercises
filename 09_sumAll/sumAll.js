648aa829cb0eb4f3e8fea568fddd12d4ce0069af
DevSergioSifuentes <dev.sergiosifuentes@gmail.com>
2025-09-09 19:47:03 -0600

    build: coded sum all integres in range.
    check every error parameter in a single if, maybe
    it could be made better.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let totalSum = 0;
+    if(num1 < 0 || num2 < 0 || Number.isInteger(num1) !== true || 
+    Number.isInteger(num2) !== true || Number.isNaN(num1) || 
+    Number.isNaN(num2)){
+        return "ERROR"
+    }else{
+        if(num2 < num1){
+            for(i = num2; i <= num1; i++){
+                totalSum += i;
+            }
+        }
+        else{
+             for(i = num1; i <= num2; i++){
+                totalSum += i;
+            }
+        }
+        return totalSum;
+    }
 };
 
 // Do not edit below this line
