91c9e16ecb4d97c5783cccc3bc0758fab942254e
DevSergioSifuentes <dev.sergiosifuentes@gmail.com>
2025-09-13 17:59:49 -0600

    build: coded a simple palindrome checker.
    It only checks alphanumerical values.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (entry) {
+    let plainString = entry.replace(/[^a-zA-Z0-9]/g,"").toLowerCase();
+    let reversedString ="";
+    for(let i=plainString.length-1;i >= 0; i--){
+        reversedString += plainString[i];
+    }
+    console.log(plainString);
+    console.log(reversedString);
 
+    if(plainString === reversedString){
+        return true;
+    }else{
+        return false;
+    }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
