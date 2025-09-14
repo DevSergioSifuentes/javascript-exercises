0298761bcc37e2bbf8c671e7585525400c0be66e
DevSergioSifuentes <dev.sergiosifuentes@gmail.com>
2025-09-13 19:35:09 -0600

    build: coded a function that returns the number at in the fibonacci series that you pass.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    intValue=parseInt(value);
+    if(intValue < 0){
+        return "OOPS";
+    }else{
+        let constructor =[0,1];
+        for(let i=2;i<=intValue; i++){
+            constructor.push(constructor[i-2]+constructor[i-1]);
 
+        }
+    
+        return constructor[intValue];
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
